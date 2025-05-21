1fea0a3224c1c5cd83ca99f6cb39cfbea0fd52d9
mbuhl <git@moritzbuhl.de>
2025-05-21 07:37:53 +0200

    Remove typo

--- sys/arch/amd64/amd64/machdep.c
+++ sys/arch/amd64/amd64/machdep.c
@@ -1,4 +1,4 @@
-/*r	$OpenBSD: machdep.c,v 1.297 2024/09/21 19:06:07 deraadt Exp $	*/
+/*	$OpenBSD: machdep.c,v 1.297 2024/09/21 19:06:07 deraadt Exp $	*/
 /*	$NetBSD: machdep.c,v 1.3 2003/05/07 22:58:18 fvdl Exp $	*/
 
 /*-
